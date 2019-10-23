93b38c9b8f43a49be986b3c9efcf16a8ebe8bd5f
Merch Visoiu <merch@merchv.com>
2019-10-23 01:13:14 -0400

    Changed directory structure to build with Swift Package Manager

--- Package.swift
+++ Package.swift
@@ -1,18 +1,11 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.1
 import PackageDescription
-
-#if os(macOS) || os(iOS) || os(watchOS) || os(tvOS)
-let dependencies: [Package.Dependency] = []
-#else
-let dependencies: [Package.Dependency] = [.package(url: "https://github.com/Meniny/Zipper.git", .exact("1.0.2"))]
-#endif
-
 let package = Package(
     name: "Zipper",
     products: [
         .library(name: "Zipper", targets: ["Zipper"])
     ],
-	dependencies: dependencies,
+	dependencies: [],
     targets: [
         .target(name: "Zipper"),
 		.testTarget(name: "ZipperTests", dependencies: ["Zipper"])
